75d1e8bc42ed7698e1fee507d34fc0a9cea077fe
Ildar <student@localhost>
2022-01-26 11:34:06 +0300

    all

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿void PrintArray(string[] args)
+{
+
+    for (int i = 0; i < args.Length; i++)
+    {
+        if (args[i].Length <= 3)
+        {
+            Console.Write(args[i] + " ");
+        }
+    }
+}
+
+string[] firstStr = { "machine", "two", "kdfj", "546", "-35", "ok", "note" };
+for (int j = 0; j < firstStr.Length; j++)
+{
+    Console.Write(firstStr[j] + " ");
+}
+Console.WriteLine();
+string[] secondStr = { };
+secondStr = firstStr;
+PrintArray(secondStr);
